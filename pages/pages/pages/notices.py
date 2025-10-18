c99f716e0a9e09d6fe1ec912ab1ee6ebf71d63c1
424 <224510559+424-tech@users.noreply.github.com>
2025-10-18 18:28:01 +0530

    Delete pages directory

--- pages/pages/pages/notices.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("📋 Notice Board")
-
-uploaded_file = st.file_uploader("Upload a notice image", type=["jpg", "png"])
-
-if uploaded_file:
-    st.image(uploaded_file, caption="Uploaded Notice")
-    st.success("Notice uploaded!")
